1167406141afe025ee8c6de5775a36baeb5b995f
Ionut Popovici <ionut.popovici@icoldo.com>
2017-12-07 20:00:18 +0200

    experiments with markupExtensions to create custom brushes (failing)

--- WPFluent/WPFluent.Demo/MainWindow.xaml.cs
+++ WPFluent/WPFluent.Demo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
+using System.Windows.Media.Composition;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -25,4 +28,36 @@ namespace WPFluent.Demo
             InitializeComponent();
         }
     }
+
+
+
+    [MarkupExtensionReturnType(typeof(Brush))]
+    public class CustomBrush : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+
+            SolidColorBrush q = new SolidColorBrush();
+            q.Color = Color.FromArgb(55, 66, 77, 88);
+
+            return q;
+        }
+
+    }
+
+    [MarkupExtensionReturnType(typeof(Brush))]
+    public class AcrylicBrush : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+
+            SolidColorBrush q = new SolidColorBrush();
+            q.Color = Color.FromArgb(55, 66, 77, 88);
+
+            return q;
+        }
+
+    }
+
+
 }
